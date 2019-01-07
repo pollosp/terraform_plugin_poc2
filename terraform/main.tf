d3406805d0d3168a640e9d3a8aa72749df06cadc
pollosp <pollo.es.pollo@gmail.com>
2019-01-07 21:21:33 +0100

    Adds iterator to main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,27 @@
+provider "google" {
+  project     = "XXXX"
+}
+
+
 data "artifact" "test1" {
 }
 
 output "artifacttype" {
   value = "${data.artifact.test1.artifacttype}"
 }
+
 output "artifactlist" {
   value = "${data.artifact.test1.artifactlist}"
 }
 
+
+locals {
+  artifactlist = "${data.artifact.test1.artifactlist}"
+}
+
+
+resource "google_storage_bucket" "from_list" {
+  count    = "${length(local.artifactlist)}"
+  name     = "${element(local.artifactlist, count.index)}"
+  location = "EU"
+}
